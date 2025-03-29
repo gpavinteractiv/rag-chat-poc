1af65c5f0eb6a004b4f7ef2c0a088edacc3fa53d
gpavinteractiv <guillaume.pavese@connectics.fr>
2025-03-29 03:01:41 +0100

    fix init & rebuild scripts

--- backend/run-backend.sh
+++
@@ -1,6 +0,0 @@
-podman run --detach \
-           --name rag-backend-container \
-           --publish 8000:8000 \
-           --volume "$(pwd)/.env:/app/.env:ro,Z" \
-           --volume "$(pwd)/../projects:/app/../projects:ro,Z" \
-           localhost/rag-chat-backend:0.1
